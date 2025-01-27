ced43d39bb7346fa5bb6aba7b739a0a985c3c2ba
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-01-27 20:24:48 +0100

    Added QlogFileSeq struct and child structs

--- src/logfile.rs
+++ src/logfile.rs
@@ -1,4 +1,13 @@
-pub struct LogFile {
+use std::collections::HashMap;
+
+use chrono::NaiveDateTime;
+
+pub struct QlogFileSeq {
+	log_file: LogFile,
+	trace: TraceSeq
+}
+
+struct LogFile {
 	file_schema: String,
 	serialization_format: String,
 	title: Option<String>,
@@ -20,4 +29,126 @@ impl LogFile {
 			event_schemas: vec!["urn:ietf:params:qlog:events:moq".to_string()]
 		}
 	}
+}
+
+struct TraceSeq {
+	title: Option<String>,
+	description: Option<String>,
+	common_fields: Option<CommonFields>,
+	vantage_point: Option<VantagePoint>,
+}
+
+struct CommonFields {
+	path: Option<PathId>,
+	time_format: Option<TimeFormat>,
+	reference_time: Option<ReferenceTime>,
+	protocol_types: Option<Vec<String>>,
+	group_id: Option<GroupId>,
+	custom_fields: HashMap<String, String>
+}
+
+type PathId = String;
+type GroupId = String;
+
+enum TimeFormat {
+	RelativeToEpoch,
+	RelativeToPreviousEvent
+}
+
+struct ReferenceTime {
+	clock_type: ClockType,
+	epoch: Epoch,
+	// TODO: Look at what to do with timezones
+	wall_clock_time: Option<NaiveDateTime>
+}
+
+enum ClockType {
+	System,
+	Monotonic,
+	Other(String)
+}
+
+enum Epoch {
+	// TODO: Look at what to do with timezones
+	Rfc3339DateTime(NaiveDateTime),
+	Unknown
+}
+
+struct VantagePoint {
+	name: Option<String>,
+	vp_type: VantagePointType,
+	flow: Option<VantagePointType> // TODO: Make required if vp_type == Network
+}
+
+enum VantagePointType {
+	/// Initiates the connection
+	Client,
+	/// Accepts the connection
+	Server,
+	/// Observer in between client and server
+	Network,
+	Unknown
+}
+
+struct Event {
+	time: f64,
+	name: String,
+	data: ProtocolEventData,
+	path: Option<PathId>,
+	time_format: Option<TimeFormat>,
+	protocol_types: Option<Vec<String>>,
+	group_id: Option<GroupId>,
+	system_info: Option<SystemInformation>,
+	custom_fields: HashMap<String, String>
+}
+
+struct SystemInformation {
+	processor_id: Option<u32>,
+	process_id: Option<u32>,
+	thread_id: Option<u32>
+}
+
+enum ProtocolEventData {
+	LogLevelEventData(LogLevelEventData)
+}
+
+enum LogLevelEventData {
+	LogLevelError(LogLevelError),
+	LogLevelWarning(LogLevelWarning),
+	LogLevelInfo(LogLevelInfo),
+	LogLevelDebug(LogLevelDebug),
+	LogLevelVerbose(LogLevelVerbose)
+}
+
+// TODO: Look at what to do with the extension (see RFC draft)
+struct LogLevelError {
+	code: Option<u64>,
+	message: Option<String>
+}
+
+// TODO: Look at what to do with the extension (see RFC draft)
+struct LogLevelWarning {
+	code: Option<u64>,
+	message: Option<String>
+}
+
+// TODO: Look at what to do with the extension (see RFC draft)
+struct LogLevelInfo {
+	message: Option<String>
+}
+
+// TODO: Look at what to do with the extension (see RFC draft)
+struct LogLevelDebug {
+	message: Option<String>
+}
+
+// TODO: Look at what to do with the extension (see RFC draft)
+struct LogLevelVerbose {
+	message: Option<String>
+}
+
+struct RawInfo {
+	length: Option<u64>,
+	payload_length: Option<u64>,
+	data: Option<Vec<u8>>
 }
